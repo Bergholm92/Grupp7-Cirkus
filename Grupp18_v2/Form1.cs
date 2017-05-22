f08b51cfda247e62e621232ee95444e6eda8a8a9
Bergholm92 <gustav@onsalait.se>
2017-05-22 19:53:48 +0200

    Skapat connection
    Har skapat en connectionstring och installerat Nugget.

--- Grupp18_v2/Form1.cs
+++ Grupp18_v2/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace Grupp18_v2
 {
     public partial class Form1 : Form
     {
+
+        NpgsqlConnection conn = new NpgsqlConnection("Server = webblabb.miun.se; Port=5432;User Id = db_g7; Password=cirkus88; Database=db_g7;");
+
         public Form1()
         {
             InitializeComponent();
