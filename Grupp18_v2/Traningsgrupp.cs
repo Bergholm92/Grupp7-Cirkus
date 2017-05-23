043d675cd76e15ed4181b610c510cb1edfcdb142
EGmU <emil.g95@hotmail.com>
2017-05-23 10:12:44 +0200

    Plats och Träningsgruppklasser
    Lade till plats och träningsgruppsklasser med props

---
+++ Grupp18_v2/Traningsgrupp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp18_v2
+{
+    class Traningsgrupp
+    {
+        public int Traningsgrupp_id { get; set; }
+        public string Namn { get; set; }
+        public int Antal { get; set; }
+        public string Typ { get; set; }
+    }
+}
