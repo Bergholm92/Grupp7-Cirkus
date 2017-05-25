e7e58f405f0c26290f6251ea8264a9099df4d557
EGmU <emil.g95@hotmail.com>
2017-05-25 12:09:31 +0200

    Jadu, vetefan

--- Grupp18_v2/LedareForm.Designer.cs
+++ Grupp18_v2/LedareForm.Designer.cs
@@ -30,14 +30,15 @@
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(244, 12);
+            this.listBox1.Location = new System.Drawing.Point(272, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(214, 264);
+            this.listBox1.Size = new System.Drawing.Size(186, 264);
             this.listBox1.TabIndex = 0;
             // 
             // listBox2
@@ -49,11 +50,21 @@
             this.listBox2.TabIndex = 1;
             this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(502, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Närvaro";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // LedareForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(910, 499);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
             this.Name = "LedareForm";
@@ -66,5 +77,6 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
